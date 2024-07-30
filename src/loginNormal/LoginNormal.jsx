c41ea46e2d6092edfcf6f9791a0502a795a16740
Pei <>
2024-07-31 00:56:45 +0800

    add normal & vendor  login page

---
+++ src/loginNormal/LoginNormal.jsx
@@ -0,0 +1,28 @@
+import "bootstrap-icons/font/bootstrap-icons.css";
+import LoginForm from "../components/LoginForm";
+
+const LoginNormal = () => {
+  return (
+    <>
+      <div className="p-5 ">
+        <div className=" d-flex rounded-5 overflow-hidden ">
+          {/* 一般用戶 */}
+          <div className="bg-primary w-50 f-col-j-sa-a-center p-5 ">
+            <img className="w-25 f-center" src="images/icon/stall.png" alt="" />
+            <p className="text-center w-50">
+              探索創意，感受熱情 享受與創作者交流的美好時光
+            </p>
+            <i className="bi bi-arrow-left-circle fs-1 fw-bolder c-white"></i>
+          </div>
+
+          {/* 登入 */}
+
+          <div className="bg-white w-50 f-col-center p-5  bg-lightGray">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default LoginNormal;
