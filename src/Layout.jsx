ff097f5d1c81ddf9ffcdf371456d6ad6d00ae173
ChingWenWen <box789987@gmail.com>
2024-07-31 15:32:45 +0800

    set up routes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,32 +1,37 @@
 import React from "react";
 import styles from "./Layout.module.scss";
-// import Home from "./home/Home";
-// import News from "./news/News";
-// import AllVendors from "./allVendor/AllVendors";
-// import Vendor from "./vendor/Vendor";
-// import Type from "./type/Type";
+import Home from "./home/Home";
+import News from "./news/News";
+import AllVendors from "./allVendor/AllVendors";
+import Vendor from "./vendor/Vendor";
+import Type from "./type/Type";
 import Map from "./map/Map";
-// import MemberIndexNormal from "./memberIndexNormal/MemberIndexNormal";
-// import MemberIndexVendor from "./memberIndexVendor/MemberIndexVendor";
-// import Login from "./login/Login";
-// import LoginNormal from "./loginNormal/LoginNormal";
-// import LoginVendor from "./loginVendor/LoginVendor";
+import MemberIndexNormal from "./memberIndexNormal/MemberIndexNormal";
+import MemberIndexVendor from "./memberIndexVendor/MemberIndexVendor";
+import Login from "./login/Login";
+import LoginNormal from "./loginNormal/LoginNormal";
+import LoginVendor from "./loginVendor/LoginVendor";
 
+import { Routes, Route, useLocation } from 'react-router-dom';
 // TODO:
 // 1. 把 Home 搬進來、Layout 搬進 App（注意路徑改變）
 // 2. 加上背景
 // 3. 加上 React Router（先不做）
 const Layout = () => {
+  const location = useLocation();
   return (
     <div className="layout">
       <div className="position-relative">
         <div className={styles.background}>
-          {/* <Home /> */}
-          {/* <News /> */}
-          {/* <AllVendors /> */}
-          {/* <Vendor /> */}
-          {/* <Type /> */}
-          <Map />
+          <Routes location={location}>
+            <Route path="/" element={<Home />} />
+            <Route path="/haoshih" element={<Home />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/shop" element={<AllVendors />} />
+            <Route path="/vendor" element={<Vendor />} />
+            <Route path="/type" element={<Type />} />
+            <Route path="/map" element={<Map />} />
+          </Routes>
           {/* <MemberIndexNormal /> */}
           {/* <MemberIndexVendor /> */}
         </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,21 +2,21 @@ const data = [
   {
     id: 1,
     text: "最新消息",
-    src: "http",
+    src: "/news",
     hoverText: `市集公告、攤位招
     租、最新活動等相𨶹資訊`,
   },
   {
     id: 2,
     text: "攤販類型",
-    src: "http",
+    src: "/type",
     hoverText: `介紹好尸、集內的
 多元攤販`,
   },
   {
     id: 3,
     text: "市集地圖",
-    src: "http",
+    src: "/map",
     hoverText: `提供市集導覽與交通資訊，帶領你們
 走進好尸、集`,
   },
@@ -35,7 +35,7 @@ const NavBar = () => {
       <div className="navBar">
         <nav className="navbar navbar-expand p-3">
           <div className="container-fluid">
-            <a href="https://首頁">
+            <a href="/haoshih">
               <img className="pe-auto" src="images/img/logo.png" alt="logo" />
             </a>
             <div className="d-flex flex-row">
@@ -43,14 +43,14 @@ const NavBar = () => {
                 <a
                   key={item.id}
                   className={`text-decoration-none c-black fs-5 px-3 py-2 hover-bg-secondary rounded-pill`}
-                  href="https"
+                  href={item.src}
                 >
                   {item.text}
                 </a>
               ))}
             </div>
             <div className="rounded-pill fs-5 px-3 py-2  bg-primary c-white hover-bg-blueGray cursor-point ">
-              <a className="link-light text-decoration-none" href="https:">
+              <a className="link-light text-decoration-none" href="/shop">
                 市集商城
               </a>
             </div>
