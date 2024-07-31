c474420b51a99b60e7ccb16863cd7728987d0f49
Pei <>
2024-08-01 02:10:50 +0800

    add

--- src/shopCart/ShopList.jsx
+++ src/shopCart/ShopList.jsx
@@ -3,35 +3,40 @@ import { Form, Button, Image, InputGroup } from "react-bootstrap";
 
 const ShopList = () => {
   return (
-    <>
-      <tr>
-        <td>
-          <Form.Check type="checkbox" />
-        </td>
-        <td>
-          <Image
-            className="img-w100"
-            src="https://tokyo-kitchen.icook.network/uploads/recipe/cover/326709/ba0a4f1dfb7aad9e.jpg"
-            thumbnail
-            style={{ width: "100px" }}
-          />
-          <span className="ml-2">商品名字1</span>
-        </td>
-        <td>NT$ 999</td>
-        <td>
-          <InputGroup style={{ width: "120px" }}>
-            <Button variant="outline-secondary">-</Button>
-            <Form.Control
-              type="text"
-              value="12"
-              className="text-center border-secondary"
+    <tr>
+      <td className="text-center align-middle">
+        <Form.Check type="checkbox" />
+      </td>
+      <td className="text-center">
+        <div className="d-flex align-items-center justify-content-center">
+          <div
+            className="overflow-hidden rounded"
+            style={{ width: "100px", height: "100px" }}
+          >
+            <Image
+              src="https://tokyo-kitchen.icook.network/uploads/recipe/cover/326709/ba0a4f1dfb7aad9e.jpg"
+              alt="商品圖片"
+              className="w-100 h-100 object-fit-cover"
             />
-            <Button variant="outline-secondary">+</Button>
-          </InputGroup>
-        </td>
-        <td>NT$ 240</td>
-      </tr>
-    </>
+          </div>
+          <span className="ms-3">商品名字1</span>
+        </div>
+      </td>
+      <td className="text-center align-middle">NT$ 999</td>
+      <td className="text-center align-middle">
+        <InputGroup className="mx-auto" style={{ width: "120px" }}>
+          <Button variant="outline-secondary">-</Button>
+          <Form.Control
+            type="text"
+            value="12"
+            className="text-center border-secondary"
+          />
+          <Button variant="outline-secondary">+</Button>
+        </InputGroup>
+      </td>
+      <td className="text-center align-middle">NT$ 240</td>
+    </tr>
   );
 };
+
 export default ShopList;
