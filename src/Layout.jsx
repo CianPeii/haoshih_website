b007451986d555e9dc416bddb160a687d1970340
Pei <>
2024-07-31 15:05:25 +0800

    add shopCart Page

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -5,12 +5,13 @@ import styles from "./Layout.module.scss";
 // import AllVendors from "./allVendor/AllVendors";
 // import Vendor from "./vendor/Vendor";
 // import Type from "./type/Type";
-import Map from "./map/Map";
+// import Map from "./map/Map";
 // import MemberIndexNormal from "./memberIndexNormal/MemberIndexNormal";
 // import MemberIndexVendor from "./memberIndexVendor/MemberIndexVendor";
 // import Login from "./login/Login";
 // import LoginNormal from "./loginNormal/LoginNormal";
 // import LoginVendor from "./loginVendor/LoginVendor";
+import ShopCart from "./shopCart/ShopCart";
 
 // TODO:
 // 1. 把 Home 搬進來、Layout 搬進 App（注意路徑改變）
@@ -26,9 +27,10 @@ const Layout = () => {
           {/* <AllVendors /> */}
           {/* <Vendor /> */}
           {/* <Type /> */}
-          <Map />
+          {/* <Map /> */}
           {/* <MemberIndexNormal /> */}
           {/* <MemberIndexVendor /> */}
+          <ShopCart />
         </div>
       </div>
       {/* <LoginNormal /> */}
