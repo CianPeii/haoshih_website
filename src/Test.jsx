ae9245068f5ce4239e1063bb67e5f9003ce199f4
Pei <>
2024-08-02 01:12:43 +0800

    no message

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,31 +1,41 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 const Test = () => {
+  const [count, setCount] = useState(1);
 
-    const [count, setCount] = useState(1)
+  useEffect(() => {
+    console.log("effect func");
 
-    useEffect(() => {
-        console.log('effect func');
+    let timer;
+    timer = setInterval(() => {
+      console.log(count);
+    }, count * 1000);
 
-        let timer
-        timer = setInterval(()=>{
-            console.log(count);
-        }, count * 1000)
+    return () => {
+      console.log("clear func");
+      clearInterval(timer);
+    };
+  }, [count]);
 
-        return () => {
-            console.log('clear func');
-            clearInterval(timer)
-        }
-    }, [count])
-    
-    
-
-    return <>
-    <button onClick={()=>{setCount(prev=>--prev)}}>-</button>
-    <div>{count}</div>
-    <button onClick={()=>{setCount(prev=>++prev)}}>+</button>
+  return (
+    <>
+      <button
+        onClick={() => {
+          setCount((prev) => --prev);
+        }}
+      >
+        -
+      </button>
+      <div>{count}</div>
+      <button
+        onClick={() => {
+          setCount((prev) => ++prev);
+        }}
+      >
+        +
+      </button>
     </>
-  
-}
+  );
+};
 
-export default Test
+export default Test;
