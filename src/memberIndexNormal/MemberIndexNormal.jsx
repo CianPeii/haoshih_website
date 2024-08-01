f691d1db66434c678c0547df5be2743329eabed1
Qing <haru61410@gmail.com>
2024-08-01 19:20:06 +0800

    got data from api

--- src/memberIndexNormal/MemberIndexNormal.jsx
+++ src/memberIndexNormal/MemberIndexNormal.jsx
@@ -3,7 +3,30 @@ import MemberSideBar from "../components/MemberSideBar";
 import SubTitleYellow from "../components/SubTitleYellow";
 import MemberForm from "../components/MemberForm";
 import ChatBtn from "../components/ChatBtn";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const MemberIndexNormal = () => {
+  const [memberData, setMemberData] = useState(null);
+
+  useEffect(() => {
+    const fetchMemberData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:3200/api/member/profile/2"
+        );
+        setMemberData(response.data);
+        // console.log("Member Data:", response.data); // 數據首次被獲取時在控制台顯示
+      } catch (error) {
+        console.error("Error fetching member data:", error);
+      }
+    };
+
+    fetchMemberData();
+  }, []); // 空陣列表示這個效果只在組件首次渲染時運行
+
+  // console.log("Current memberData state:", memberData); // 每次重新渲染時顯示當前的 memberData 狀態
+
   return (
     <>
       <NavBarShop />
@@ -13,7 +36,8 @@ const MemberIndexNormal = () => {
         </div>
         <div className="col-10 ">
           <SubTitleYellow title="會員資料" />
-          <MemberForm />
+
+          {memberData ? <MemberForm profile={memberData} /> : <p>Loading...</p>}
 
           <ChatBtn />
         </div>
@@ -23,3 +47,20 @@ const MemberIndexNormal = () => {
 };
 
 export default MemberIndexNormal;
+
+// const [profile, setProfile] = useState({
+//   uid: 1,
+//   account: "mingming0916",
+//   password: "123456",
+//   first_name: "王",
+//   last_name: "曉明",
+//   nickname: "煞氣a明",
+//   phone: "0911222333",
+//   address: "台中市南屯區公益路二段51號18樓",
+//   email: "demo@gmail.com",
+//   tw_id: "A123456789",
+// });
+
+{
+  /* <MemberForm profile={profile} /> */
+}
