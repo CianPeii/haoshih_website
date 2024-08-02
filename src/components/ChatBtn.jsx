dc2c65b06df52f83476c65417cb9dcfdce095a6b
Pei <n0988040989@gmail.com>
2024-08-02 10:03:36 +0800

    ChatBtn size

--- src/components/ChatBtn.jsx
+++ src/components/ChatBtn.jsx
@@ -2,7 +2,7 @@ const ChatBtn = () => {
   return (
     <>
       <i
-        className="rounded-circle position-fixed p-3 c-white bg-blueGray d-inline bi bi-chat-dots fs-2 opacity-75"
+        className="rounded-circle position-fixed p-3 c-white bg-blueGray d-inline bi bi-chat-dots fs-3 opacity-75"
         style={{ bottom: "10%", right: "2%" }}
       ></i>
     </>
