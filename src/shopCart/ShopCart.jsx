bb6d7e6cfaeca2595d6fbd54997e6088abc9a5aa
Pei <n0988040989@gmail.com>
2024-08-03 04:05:16 +0800

    Modify details

--- src/shopCart/ShopCart.jsx
+++ src/shopCart/ShopCart.jsx
@@ -3,7 +3,7 @@ import { Button } from "react-bootstrap";
 import MemberSideBar from "../components/MemberSideBar";
 import NavBarShop from "../components/NavBarShop";
 import SubTitleYellow from "../components/SubTitleYellow";
-import ShopingCartForm from "./ShopingCartForm";
+import ShopCartForm from "./ShopCartForm";
 import Footer from "../components/Footer";
 
 const ShopCart = () => {
@@ -17,8 +17,8 @@ const ShopCart = () => {
         <div className="col-10 ">
           <SubTitleYellow title="購物車" />
           <div className="container">
-            <ShopingCartForm />
-            <ShopingCartForm />
+            <ShopCartForm />
+            <ShopCartForm />
             <div className="f-end-end mt-5 gap-3 ">
               <h4>總金額：NT$999</h4>
               <Button variant="danger rounded-pill px-4 py-2">前往結帳</Button>
