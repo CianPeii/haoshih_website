6d39d6e7a5cecc6602b4a482b9768cae97624733
Pei <n0988040989@gmail.com>
2024-08-05 22:26:38 +0800

    change NavBarShop to SetStallls

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -14,6 +14,7 @@ import LoginVendor from "./loginVendor/LoginVendor";
 import ShopCart from "./shopCart/ShopCart";
 import Normal from "./SignUp/Normal";
 import SetStalls from "./setStalls/SetStalls";
+import Default from "./default/Default";
 
 import { Routes, Route, useLocation } from "react-router-dom";
 
@@ -38,7 +39,7 @@ const Layout = () => {
             <Route path="/SetStalls" element={<SetStalls />} />
             {/* <Route path="/member/:uid" element={<MemberIndexNormal />} /> */}
             {/* <Route path="/vendor/:vid" element={<MemberIndexVendor />} /> */}
-            TODO: default route
+            TODO: default route =- 已經在 default/Default.jsx
           </Routes>
           {/* <ShopCart /> */}
           {/* <Step1 /> */}
