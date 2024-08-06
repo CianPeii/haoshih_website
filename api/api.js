abb51cdf1d97bd4d441876faabe5145fbdaafa1c
ChingWenWen <box789987@gmail.com>
2024-08-06 12:03:12 +0800

    create router-login member

--- api/api.js
+++ api/api.js
@@ -7,32 +7,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-// 連接mysql
-// var mysql = require("mysql");
-// var conn = mysql.createConnection({
-//   user: "root",
-//   password: "",
-//   host: "localhost",
-//   port: 3306,
-//   database: "tododb"
-// });
-
-// conn.connect((err) => {
-//   if (err) {
-//     console.log("MySQL連線失敗");
-//     return;
-//   } else {
-//     console.log("MySQL連線成功");
-//   }
-// });
-
-// app.set('connection', conn)
-
 var cartRouter = require("./cartRouter.js")
 app.use("/carts", cartRouter)
 
-// var loginRouter = require("./loginRouter.js")
-// app.use("/login", loginRouter)
+var loginRouter = require("./loginRouter.js")
+app.use("/login", loginRouter)
 
 var mapRouter = require("./mapRouter.js")
 app.use("/map", mapRouter)
