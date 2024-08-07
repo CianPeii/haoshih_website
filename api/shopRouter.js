a84fad637c66bb8685cf128b2b9b79a1363705f7
ChingWenWen <box789987@gmail.com>
2024-08-07 10:22:12 +0800

    update-add shop api

--- api/shopRouter.js
+++ api/shopRouter.js
@@ -13,11 +13,34 @@ var conn = config.connection
 // })
 // --------測試路由用----------
 
-
+// 所有攤販類型
 shopRouter.get("/",function (req,res) {
     conn.query('select * from vendor_info',function (err, result) {
         res.json(result)
     })
 })
+
+// 切換特定攤販類型
+shopRouter.get("/:type",function (req,res) {
+    conn.query('select * from vendor_info where brand_type = ?',[req.params.type],function (err, result) {
+        res.json(result)
+    })
+})
+
+// 取得該攤販的商品
+shopRouter.get('/:vinfo/products',function (req,res) {
+    conn.query('select * from product where vid = ?',[req.params.vinfo],function (err, result) {
+        res.json(result)
+    })
+})
+
+// 取得單件商品
+shopRouter.get("/product/:pid",function (req, res) {
+    conn.query('select * from product where pid = ?',[req.params.pid],function (err, result) {
+        res.json(result)
+    })
+})
+
+// 加入購物車(目前在cartRouter，路徑還未處理)
 
 module.exports = shopRouter
