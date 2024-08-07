bfeaf63de3faab138a8f802d4e1bec3725887ea8
ChingWenWen <box789987@gmail.com>
2024-08-07 16:09:29 +0800

    rendor vendorType

--- src/allVendor/components/AllVendorsCard.jsx
+++ src/allVendor/components/AllVendorsCard.jsx
@@ -1,22 +1,23 @@
 import styles from "./AllVendorsCard.module.scss";
-const AllVendorsCard = () => {
+import { Buffer } from "buffer";
+const AllVendorsCard = ({data}) => {
+  var imgBlob = Buffer.from(data.brand_img01).toString('base64')
   return (
     <>
       <div className="col-3">
         <div className={`card p-3 mx-2  my-3 ${styles.cardBg}`}>
           <img
             className={styles.cardImg}
-            src="https://www.designevo.com/res/templates/thumb_small/sweet-chocolate-doughnut.webp"
+            src={"data:image/jpeg;base64,"+imgBlob}
             alt=""
           />
           <div className="card-body">
-            <h4 className="card-title">店家名稱</h4>
+            <h4 className="card-title">{data.brand_name}</h4>
             <p className={`card-text ${styles.cardText}`}>
-              店家介紹，最多兩行最多兩行最多兩行
-              店家介紹，最多兩行最多兩行最多兩行
+              {data.content}
             </p>
-            <div className={styles.cardTag}>#標籤限制六字</div>
-            <div className={styles.cardTag}>#最多只能六字</div>
+            <div className={styles.cardTag}>#{data.tag1||"美好市集"}</div>
+            <div className={styles.cardTag}>#{data.tag2||"美好市集"}</div>
           </div>
         </div>
       </div>
--- src/allVendor/components/Sections.jsx
+++ src/allVendor/components/Sections.jsx
@@ -1,13 +1,13 @@
 import SectionBtn from "./SectionBtn";
 
 const title = ["全部", "服飾", "飾品", "手作", "美食", "寵物", "其他"];
-
-const Sections = () => {
+const en_title = ["all", "clothing", "accessories", "handmade", "food", "pet", "others"]
+const Sections = ({ type, changeType }) => {
   return (
     <>
       <div className="p-5 d-flex ">
         {title.map((item, index) => (
-          <SectionBtn key={index} title={item} />
+          <SectionBtn key={index} title={item} type={en_title[index]} changeType={changeType} />
         ))}
       </div>
     </>
