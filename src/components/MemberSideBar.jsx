23c3e5056fd3a3064066859dc744486b750f81e5
Qing <haru61410@gmail.com>
2024-08-08 23:45:15 +0800

    add sideBar link

--- src/components/MemberSideBar.jsx
+++ src/components/MemberSideBar.jsx
@@ -6,7 +6,9 @@ const MemberSideBar = () => {
         <div className="d-flex flex-column align-items-start">
           <h1 className="mb-5 fs-1">會員專區</h1>
           <div className={`w-100 ${styles.sideBarSubTitle}`}>
-            <h4>會員資料</h4>
+            <a href="http://localhost:3000/member/1">
+              <h4>會員資料</h4>
+            </a>
             <h4>我的訂單</h4>
             <h4>按讚攤位</h4>
             <h4>聊天室</h4>
--- src/memberIndexVendor/components/VendorStallInfo.jsx
+++ src/memberIndexVendor/components/VendorStallInfo.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 const VendorStallInfo = () => {
-  return <div>VendorStallInfo</div>;
+  return (
+    <>
+      <h1>VendorStallInfo</h1>
+      <div className="d-flex justify-content-center">
+        <Button
+          className="me-5"
+          variant="bg-white border border-2 c-gray rounded-pill px-4 py-2"
+          type="button"
+          onClick={() => navigate(`/vendor/1/vendorInfo`)}
+        >
+          取消變更
+        </Button>
+        <Button
+          className="ms-5"
+          variant=" bg-blueGray text-white rounded-pill px-4 py-2"
+          type="submit"
+          onClick={async () => {
+            await refetch(); // 先執行 refetch
+            navigate(`/vendor/1/vendorInfo`); // 然後導航
+          }}
+        >
+          儲存變更
+        </Button>
+      </div>
+    </>
+  );
 };
 
 export default VendorStallInfo;
