ab04bf699ef9ab58da18a5fd8e325ddca2c8548e
Leo <q94120@gmail.com>
2024-08-09 16:41:56 +0800

    0809route調整完成

--- api/cartRouter.js
+++ api/cartRouter.js
@@ -13,7 +13,7 @@ var conn = config.connection
 // })
 // --------測試路由用----------
 
-// 加入購物車
+//加入購物車
 cartRouter.post('/index', function(req, res) {
     conn.query("SELECT * FROM carts WHERE uid = ? AND pid = ?", 
         [req.body.uid, req.body.pid], function(err, results) {
@@ -34,68 +34,33 @@ cartRouter.post('/index', function(req, res) {
                     return res.status(500).send('Insert error');
                 }
                 // res.send('INSERT INTO!');
-                
             });
         }
-        res.redirect('/index/carts');
+        // res.redirect('/index/carts');
     });
 });
 
-
 cartRouter.get('/:uid', function(req, res) {
-    const conn = req.app.get('connection')
+    // const conn = req.app.get('connection') ???????????????????????????????????
     conn.query(
-        "SELECT carts.uid, carts.pid, product.name, quantity, price, img01, amount FROM carts JOIN product on carts.pid = product.pid where uid = ?",
+        "SELECT carts.uid, carts.pid, product.name, quantity, price, img01, amount, vendor_info.vinfo, brand_name FROM carts JOIN product ON carts.pid = product.pid JOIN vendor ON product.vid = vendor.vid JOIN vendor_info ON vendor.vinfo = vendor_info.vinfo WHERE uid = ?",
         [req.params.uid],
         function(err, result) {
             // console.log(result);
-            const total = calculateTotal(result);
-            res.render('indexcartdetail.ejs',{products: result, turnPrice: turnPrice, total:total});
+            res.json(result);
+            // res.render('indexcartdetail.ejs',{products: result, turnPrice: turnPrice, total:total});
         }
     )
 })
 
-function turnPrice(price) {
-    return Number(price).toLocaleString('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0
-    });
-}
-
-function calculateTotal(products) {
-    let total = 0;
-    products.forEach(product => {
-        total += product.amount * product.price;
-    });
-    return total;
-}
-
-// function updateTotal() {
-//     let total = 0;
-//     // 選取所有的 .subtotal 元素
-//     const subtotals = document.querySelectorAll('.subtotal');
-//     // 遍歷每個 .subtotal 元素
-//     subtotals.forEach(function(element) {
-//         // 獲取文本內容並移除非數字字符
-//         let priceText = element.textContent.replace(/[^\d]/g, '');
-//         // 將處理後的文本轉換為整數並加到總金額中
-//         total += parseInt(priceText, 10);
-//     });
-//     // 更新 #totalPrice 元素的文本
-//     document.getElementById('totalPrice').textContent = '總金額 : ' + turnPrice(`${total}`) + '元';
-// }
-
-// function updateTotal(products) {
-//     let total = 0;
-//     console.log(products);
-//     products.forEach(product => {
-//         total += product.amount * product.price;
-//     });
-//     return total;
-// }
-
-
+cartRouter.get('/products/:pid/:uid', function(req, res) {
+    conn.query(
+        "SELECT product.name, quantity, price, img01, vendor_info.vinfo, brand_name FROM product JOIN vendor ON product.vid = vendor.vid JOIN vendor_info ON vendor.vinfo = vendor_info.vinfo WHERE pid = ? ",
+        [req.params.pid],
+        function(err, result) {
+            res.json(result);
+        }
+    )
+})
 
 module.exports = cartRouter
