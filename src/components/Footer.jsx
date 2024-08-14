1dd61c207f08e0b19118b935f6f2f2ebf519fbbf
Pei <n0988040989@gmail.com>
2024-08-14 14:40:37 +0800

    修改付款步驟34

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -33,7 +33,7 @@ const Footer = () => {
 
             <div className="d-flex flex-column">
               <div>
-                <div className="pb-2" onClick={() => openLink("/map")}>
+                <div className="pb-2" onClick={() => openLink("/mapå")}>
                   <i className="bi bi-bus-front" />
                   <span className="fw-light">交通資訊</span>
                 </div>
