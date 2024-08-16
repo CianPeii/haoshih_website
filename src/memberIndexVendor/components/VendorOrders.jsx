7f690a4aec4270858f439e95504635929180f5fc
Qing <haru61410@gmail.com>
2024-08-16 18:41:42 +0800

    調整攤主-訂單管理

--- src/memberIndexVendor/components/VendorOrders.jsx
+++ src/memberIndexVendor/components/VendorOrders.jsx
@@ -6,6 +6,7 @@ import VendorOrderCard from "./VendorOrderCard";
 const VendorOrders = (props) => {
   // 這裡拿到的是訂單陣列
   const { orderData } = props;
+  const orderFilter = (status) => (order) => order.status === status;
 
   return (
     <>
@@ -26,113 +27,71 @@ const VendorOrders = (props) => {
               : "尚無訂單"}
           </Container>
         </Tab>
+
+        {/* 只過濾一次的方式 ==> 效能會比較好 */}
+        {/* <Tab eventKey="status0" title="待付款">
+          {(() => {
+            const filteredOrders = orderData
+              ? orderData.filter(orderFilter("待付款"))
+              : [];
+            return filteredOrders.length > 0
+              ? filteredOrders.map((order) => (
+                  <VendorOrderCard key={order.oid} orderData={order} />
+                ))
+              : "目前沒有待付款訂單";
+          })()}
+        </Tab> */}
+
         <Tab eventKey="status0" title="待付款">
-          目前沒有待付款訂單
+          {orderData && orderData.length > 0
+            ? (() => {
+                const filteredOrders = orderData.filter(orderFilter("待付款"));
+                return filteredOrders.length > 0
+                  ? filteredOrders.map((order) => (
+                      <VendorOrderCard key={order.oid} orderData={order} />
+                    ))
+                  : "目前沒有待付款訂單";
+              })()
+            : "正在加載訂單..."}
         </Tab>
+        {/* 以下文字顯示邏輯須修正，不能以是否有orderData作為判斷 */}
         <Tab eventKey="status1" title="待出貨">
-          Tab content for Loooonger Tab
+          {orderData
+            ? orderData
+                .filter(orderFilter("待出貨"))
+                .map((order) => (
+                  <VendorOrderCard key={order.oid} orderData={order} />
+                ))
+            : "目前沒有待出貨訂單"}
         </Tab>
         <Tab eventKey="status2" title="已出貨">
-          目前沒有已出貨訂單
+          {orderData
+            ? orderData
+                .filter(orderFilter("已出貨"))
+                .map((order) => (
+                  <VendorOrderCard key={order.oid} orderData={order} />
+                ))
+            : "目前沒有已出貨訂單"}
         </Tab>
         <Tab eventKey="status3" title="待收貨">
-          目前沒有待出貨訂單
+          {orderData
+            ? orderData
+                .filter(orderFilter("待收貨"))
+                .map((order) => (
+                  <VendorOrderCard key={order.oid} orderData={order} />
+                ))
+            : "目前沒有待出貨訂單"}
         </Tab>
         <Tab eventKey="status4" title="已完成">
-          Tab content for Loooonger Tab
+          {orderData
+            ? orderData
+                .filter(orderFilter("已完成"))
+                .map((order) => (
+                  <VendorOrderCard key={order.oid} orderData={order} />
+                ))
+            : "尚無已完成訂單"}
         </Tab>
       </Tabs>
-
-      {/* <Nav justify variant="tabs" defaultActiveKey="allStatus">
-        <Nav.Item>
-          <Nav.Link
-            className="text-decoration-none c-black"
-            eventKey="allStatus"
-            data-bs-target="allStatusText"
-          >
-            全部
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link
-            className="text-decoration-none c-black"
-            eventKey="status0"
-            data-bs-target="status0Text"
-          >
-            待付款
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link className="text-decoration-none c-black" eventKey="status1">
-            待出貨
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link className="text-decoration-none c-black" eventKey="status2">
-            已出貨
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link className="text-decoration-none c-black" eventKey="status3">
-            待收貨
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link className="text-decoration-none c-black" eventKey="status4">
-            已完成
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
-      <div className="tab-content" id="myTabContent">
-        <div
-          className="tab-pane fade show active"
-          id="allStatusText"
-          role="tabpanel"
-          tabindex="0"
-        >
-          ...1
-        </div>
-        <div
-          className="tab-pane fade"
-          id="status0Text"
-          role="tabpanel"
-          tabindex="0"
-        >
-          ...2
-        </div>
-        <div
-          className="tab-pane fade"
-          id="status1Text"
-          role="tabpanel"
-          tabindex="0"
-        >
-          ...3
-        </div>
-        <div
-          className="tab-pane fade"
-          id="status2Text"
-          role="tabpanel"
-          tabindex="0"
-        >
-          ...4
-        </div>
-        <div
-          className="tab-pane fade"
-          id="status3Text"
-          role="tabpanel"
-          tabindex="0"
-        >
-          ...5
-        </div>
-        <div
-          className="tab-pane fade"
-          id="status4Text"
-          role="tabpanel"
-          tabindex="0"
-        >
-          ...6
-        </div>
-      </div> */}
     </>
   );
 };
