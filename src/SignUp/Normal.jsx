02e855c0a5c2a80aa35d7c2dbb9db1a0e67be4e4
Pei <n0988040989@gmail.com>
2024-08-17 17:39:52 +0800

    資料夾名稱錯誤 singUp=>signUp

--- src/SignUp/Normal.jsx
+++ src/SignUp/Normal.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import Footer from "../components/Footer";
 import NavBarShop from "../components/NavBarShop";
 import NormalForm from "./components/NormalForm";
-import SingUpMain from "./components/SingUpMain";
+import SignUpMain from "./components/SignUpMain";
 
 const Normal = () => {
   const navigate = useNavigate();
 
   const doRegSuccess = () => {
     // console.log('註冊成功');
-    alert('註冊成功！');
-    navigate('/login'); // 回登入頁面
+    alert("註冊成功！");
+    navigate("/login"); // 回登入頁面
   };
   return (
     <>
       <NavBarShop />
-      <SingUpMain title="一般會員註冊" />
-      <div className="f-center p-5 border">
-        <NormalForm onRegSuccess={doRegSuccess}/>
+      <SignUpMain title="一般會員註冊" />
+      <div className="f-center p-5 ">
+        <NormalForm onRegSuccess={doRegSuccess} />
       </div>
       <Footer />
     </>
--- src/SignUp/VendorSingUp.jsx
+++ src/SignUp/VendorSingUp.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import Footer from "../components/Footer";
 import NavBarShop from "../components/NavBarShop";
 import VendorSignUpForm from "./components/VendorSignUpForm";
-import SingUpMain from "./components/SingUpMain";
+import SignUpMain from "./components/SignUpMain";
 
 const VendorSignUp = () => {
   const navigate = useNavigate();
 
   const doRegSuccess = () => {
-    console.log('攤販註冊成功');
-    alert('攤販註冊成功！');
-    navigate('/login');
+    console.log("攤販註冊成功");
+    alert("攤販註冊成功！");
+    navigate("/login");
   };
 
   return (
     <>
       <NavBarShop />
-      <SingUpMain title="攤販會員註冊" />
-      <div className="f-center p-5 border">
-        <VendorSignUpForm onRegSuccess={doRegSuccess}/>
+      <SignUpMain title="攤販會員註冊" />
+      <div className="f-center p-5">
+        <VendorSignUpForm onRegSuccess={doRegSuccess} />
       </div>
       <Footer />
     </>
